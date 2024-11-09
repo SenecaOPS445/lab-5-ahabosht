34cea63678cb81b0e12feb78178347e009f1bf4e
ahabosht <ahabosht@myseneca.ca>
2024-11-09 18:44:42 -0500

    lab5b Done

--- lab5b.py
+++ lab5b.py
@@ -2,26 +2,35 @@
 #Student ID: 127098218
 
 def read_file_string(file_name):
-    with open(file_name, 'r') as file:
-        return file.read()
+    f = open(file_name, 'r')
+    data = f.read()
+    f.close()
+    return data
 
 def read_file_list(file_name):
-    with open(file_name, 'r') as file:
-        return [line.strip() for line in file]
+    f = open(file_name, 'r')
+    data = [line.strip() for line in f]
+    f.close()
+    return data
 
 def append_file_string(file_name, string_of_lines):
-    with open(file_name, 'a') as file:
-        file.write(string_of_lines)
+    f = open(file_name, 'a')
+    f.write(string_of_lines)
+    f.close()
 
 def write_file_list(file_name, list_of_lines):
-    with open(file_name, 'w') as file:
-        for line in list_of_lines:
-            file.write(f"{line}\n")
+    f = open(file_name, 'w')
+    for line in list_of_lines:
+        f.write(line + '\n')
+    f.close()
 
 def copy_file_add_line_numbers(file_name_read, file_name_write):
-    with open(file_name_read, 'r') as file_in, open(file_name_write, 'w') as file_out:
-        for idx, line in enumerate(file_in, 1):
-            file_out.write(f"{idx}:{line}")
+    f_read = open(file_name_read, 'r')
+    f_write = open(file_name_write, 'w')
+    for line_num, line in enumerate(f_read, start=1):
+        f_write.write(f"{line_num}:{line}")
+    f_read.close()
+    f_write.close()
 
 if __name__ == '__main__':
     file1 = 'seneca1.txt'
@@ -29,16 +38,12 @@ if __name__ == '__main__':
     file3 = 'seneca3.txt'
     string1 = 'First Line\nSecond Line\nThird Line\n'
     list1 = ['Line 1', 'Line 2', 'Line 3']
-
-    open(file1, 'w').close()
-    open(file2, 'w').close()
-    open(file3, 'w').close()
-
+    
     append_file_string(file1, string1)
     print(read_file_string(file1))
-
+    
     write_file_list(file2, list1)
     print(read_file_string(file2))
-
+    
     copy_file_add_line_numbers(file2, file3)
     print(read_file_string(file3))
